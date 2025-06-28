52e5c22f4790a4990245567e52cfca807336b2b6
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-28 05:52:54 -0300

    Java - Classes, pacotes e interfaces

--- src/etapaConstrucao/PlantaCasa.java
+++ src/etapaConstrucao/PlantaCasa.java
@@ -1,6 +1,6 @@
 package etapaConstrucao;
 
-public class PlantaCasa {
+public class PlantaCasa implements Construcao {
 
     public int metragem;
     public int numeroQuartos;
@@ -8,11 +8,22 @@ public class PlantaCasa {
     public String cor;
     public String material;
 
+    @Override
     public void construir(){
         System.out.println("Metragem: " + metragem);
         System.out.println("Numero de quartos: " + numeroQuartos);
         System.out.println("Numero de banheiros: " + numeroBanheiros);
         System.out.println("Material: " + material);
+    }
+
+    @Override
+    public void pintar(String cor) {
+        System.out.println("Pintando a casa com a cor " + cor);
+    }
+
+    @Override
+    public int calcularCustoConstrucao(int custoPorMetro) {
+        return metragem + custoPorMetro;
     }
 
     public void pintar(){
